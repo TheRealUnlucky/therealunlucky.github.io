79ec9bce09fff624cc1148fa744b3972c52e83a6
TheRealUnlucky <therealunlucky@gmail.com>
2016-05-23 12:35:09 -0400

    Did stuff

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// ---------- Firebase Stuff ----------
+
+var fbConfig = {
+    apiKey: "AIzaSyCm-dkGVy3t73B1ZuVM-gtHjEk_A4218O8",
+    authDomain: "dov-website.firebaseapp.com",
+    databaseURL: "https://dov-website.firebaseio.com",
+    storageBucket: "dov-website.appspot.com",
+};
+
+firebase.initializeApp(fbConfig);
+
+// ---------- Angular Stuff ---------
+
+// Declare the main application module
+// ngSanitize = The module to handle Sanitizing the 
+var app = angular.module('app', ['ngSanitize', 'btford.markdown']);
+
+app.factory('Auth', function() {
+    return firebase.auth();
+});
+
+app.run(function($rootScope, $location, $window, Auth) {
+    
+    // Create the reference to the Firebase database
+    $rootScope.firebaseDB = firebase.database().ref();
+    
+    $rootScope.isLoggedIn = function() {
+        
+        if (Auth) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
+
+app.controller('testCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+    
+}]);
